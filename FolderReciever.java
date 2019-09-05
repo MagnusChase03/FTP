3286245b4b0f2620a10e47fe75f0c586de8a2b0f
JUICEMAN123 <43757413+JUICEMAN123@users.noreply.github.com>
2019-09-05 14:31:48 -0500

    Update FolderReciever.java

--- FolderReciever.java
+++ FolderReciever.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,7 +8,6 @@ import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 
 public class FolderReciever {
@@ -19,36 +15,25 @@ public class FolderReciever {
 	public FolderReciever(String host, int port, String originalPath) throws IOException, ClassNotFoundException, InterruptedException {
 
         Socket user = new Socket(host, port);
-        //System.out.println(user.getInputStream().available());
-        
-        //Thread.sleep(1000);
+
+        //delay to process
+        Thread.sleep(5000);
         
         InputStream in = user.getInputStream();
-        //System.out.println(in.available());
         byte[] inBytes = new byte[2000000000];
         int index = 0;
         
         while (in.available() > 0) {
         	
         	inBytes[index] = (byte) in.read();
-        	//System.out.print(inBytes[index]);
         	index++;
         	
         }
         
         Folder folder = (Folder) deserialize(inBytes);
         folder.mkdirs();
-        //process(folder);
-        
-        try {
-        	
-        	BufferedReader read = new BufferedReader(new FileReader(folder.files.get(0)));
-        	System.out.println(folder.files.get(0).hashCode());
-        	read.close();
-        	
-        } catch (Exception e) {}
-        
-        
+        process(folder);
+            
         in.close();
         user.close();
 
@@ -58,41 +43,23 @@ public class FolderReciever {
 	
 		for(File f : fol.files) {
 			
-			System.out.println(f.getTotalSpace());
-			
-			//System.out.println(f);
 			
 			if(f.getName().indexOf(".") < 0) {
 				
 				Files.createDirectories(Paths.get(f.getAbsolutePath()));
 				process((Folder)f);
 				
-			} else {
+			} else if(f instanceof FolderFile){
 				
 				
-				
-				String filename = f.getAbsolutePath();
-				
-				//new File(filename).createNewFile();
-				
-				BufferedReader in = new BufferedReader(new FileReader(f));
-				String line = "";
-				String contents = "";
-				
-				while ((line = in.readLine()) != null) {
-
-					System.out.println(line);
-		            contents += line;
-		            contents += "\n";
-
-				}
-		        BufferedWriter write = new BufferedWriter(new FileWriter(f.getAbsoluteFile()));
-		        write.write(contents);
-				
+		        BufferedWriter write = new BufferedWriter(new FileWriter(f.getAbsoluteFile()), 1);
+		        write.write(((FolderFile)f).contents);
+				write.close();
+		        
 			}
 			
 		}
-		System.out.println(fol.files.get(0));
+		//System.out.println(fol.files.get(0));
 		
 	}
 	
